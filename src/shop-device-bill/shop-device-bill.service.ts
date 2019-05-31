2710d2293c9a4240e9bc5e3bb62b6fcde3f0656a
liudonghua123 <liudonghua123@gmail.com>
2019-05-31 11:09:21 +0800

    update summary of shop-device-bill

--- src/shop-device-bill/shop-device-bill.service.ts
+++ src/shop-device-bill/shop-device-bill.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { ShopDeviceBill } from './shop-device-bill.entity';
 import { ShopDeviceBillDto } from './dto/shop-device-bill.dto';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class ShopDeviceBillService {
@@ -10,7 +11,11 @@ export class ShopDeviceBillService {
     ) { }
 
     async summary(): Promise<object> {
-        const count = await this.shopDeviceBillRepository.count();
-        return { count };
+        const [summary] = await this.shopDeviceBillRepository.sequelize.query(
+            `select
+            count(*) as count, sum(transcnt) as totalTransCnt, sum(dramt) as totalDrAmt, sum(cramt) as totalCrAmt
+            from t_shopdevice_bill`,
+            { raw: true, plain: false, type: Sequelize.QueryTypes.SELECT });
+        return summary;
     }
 }
